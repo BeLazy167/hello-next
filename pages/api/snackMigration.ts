eb8056ff017daca10a49f070416f9762e7d9ee34
Dhruv Khara <dhruv.khara@wonderbiz.in>
2022-09-16 23:52:29 +0530

    Revert "minor change"
    This reverts commit f9c1b90b6a5c1100c328a6eade07a8d5b3c0473b.

--- pages/api/snackMigration.ts
+++ pages/api/snackMigration.ts
@@ -1,28 +1,30 @@
-// import prisma from "../prisma-client";
-// import data from "./temp.json";
-// import { NextApiRequest, NextApiResponse } from "next";
-
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//     if (req.method === "GET") {
-//         console.log("Snack migration started!", data.length);
-//         const finalData = [];
-//         data.map((item) => {
-//             const { _id, googleId, __v, ...rest } = item;
-//             finalData.push(rest);
-//         });
-//         const dataindb = prisma.user
-//             .createMany({
-//                 data: finalData,
-//             })
-//             .then(async () => await prisma.$disconnect());
-//         console.log("Snack migration completed!");
-
-//         res.json(dataindb);
-//     }
-// };
+import prisma from "../prisma-client";
+import data from "./temp.json";
 import { NextApiRequest, NextApiResponse } from "next";
+
+// "_id": "62de1d3ca6f79784863508ac",
+// "googleId": "101200388091517654608",
+// "name": "Omkar Babrekar",
+// "email": "omkar.babrekar@wonderbiz.in",
+// "snack": "Samosa Chat",
+// "createdAt": "2022-07-25T04:34:04.482Z",
+// "updatedAt": "2022-07-25T04:34:04.482Z",
+// "__v": 0
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "GET") {
-        res.json({ message: "in progress" });
+        console.log("Snack migration started!", data.length);
+        const finalData = [];
+        data.map((item) => {
+            const { _id, googleId, __v, ...rest } = item;
+            finalData.push(rest);
+        });
+        const dataindb = prisma.user
+            .createMany({
+                data: finalData,
+            })
+            .then(async () => await prisma.$disconnect());
+        console.log("Snack migration completed!");
+
+        res.json(dataindb);
     }
 };
