0cb3f28c5930b6d6715cfb3918624a77805bacc3
Dhruv Khara <dhruv.khara@wonderbiz.in>
2022-09-17 00:09:39 +0530

    minor change stable

--- pages/api/snackMigration.ts
+++ pages/api/snackMigration.ts
@@ -1,22 +1,27 @@
-import prisma from "../prisma-client";
-import data from "./temp.json";
+// import prisma from "../prisma-client";
+// import data from "./temp.json";
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//     if (req.method === "GET") {
+//         console.log("Snack migration started!", data.length);
+//         const finalData = [];
+//         data.map((item) => {
+//             const { _id, googleId, __v, ...rest } = item;
+//             finalData.push(rest);
+//         });
+//         const dataindb = prisma.user
+//             .createMany({
+//                 data: finalData,
+//             })
+//             .then(async () => await prisma.$disconnect());
+//         console.log("Snack migration completed!");
+
+//         res.json(dataindb);
+//     }
+// };
+
 import { NextApiRequest, NextApiResponse } from "next";
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === "GET") {
-        console.log("Snack migration started!", data.length);
-        const finalData = [];
-        data.map((item) => {
-            const { _id, googleId, __v, ...rest } = item;
-            finalData.push(rest);
-        });
-        const dataindb = prisma.user
-            .createMany({
-                data: finalData,
-            })
-            .then(async () => await prisma.$disconnect());
-        console.log("Snack migration completed!");
-
-        res.json(dataindb);
-    }
+    res.json({ message: "Hello" });
 };
