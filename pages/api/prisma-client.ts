c20a1b4b933466cb969424864049aefe6cb8a509
Dhruv Khara <dhruv.khara@wonderbiz.in>
2022-09-17 01:21:51 +0530

    final

--- pages/api/prisma-client.ts
+++ pages/api/prisma-client.ts
@@ -13,3 +13,23 @@ export const prisma =
     });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+
+
+
+
+
+// import { PrismaClient } from '@prisma/client';
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+// export default prisma;
