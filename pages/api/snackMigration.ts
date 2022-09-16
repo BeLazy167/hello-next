a8ac3858f839c3171b39d880ef4cc189d2665bd2
Dhruv Khara <dhruv.khara@wonderbiz.in>
2022-09-17 00:06:10 +0530

    reverted

--- pages/api/snackMigration.ts
+++ pages/api/snackMigration.ts
@@ -2,14 +2,6 @@ import prisma from "../prisma-client";
 import data from "./temp.json";
 import { NextApiRequest, NextApiResponse } from "next";
 
-// "_id": "62de1d3ca6f79784863508ac",
-// "googleId": "101200388091517654608",
-// "name": "Omkar Babrekar",
-// "email": "omkar.babrekar@wonderbiz.in",
-// "snack": "Samosa Chat",
-// "createdAt": "2022-07-25T04:34:04.482Z",
-// "updatedAt": "2022-07-25T04:34:04.482Z",
-// "__v": 0
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "GET") {
         console.log("Snack migration started!", data.length);
