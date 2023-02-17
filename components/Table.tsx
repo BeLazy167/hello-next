be6acdf915d1abc8733e016386c90afd128c9346
Dhruv Khara <dhruv.khara@wonderbiz.in>
2023-02-17 19:02:19 +0530

    minor bug fixes

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,64 +1,48 @@
 import { useMemo } from "react";
 import { useTable } from "react-table";
-import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-} from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td } from "@chakra-ui/react";
 export default function TableX({ userData }) {
-    if (userData) {
-        const columnsNonMemo = Object.keys(userData[0]).map((key) => {
-            return {
-                Header: key,
-                accessor: key,
-            };
-        });
-        const columns = useMemo(() => columnsNonMemo, []);
-        const data = useMemo(() => userData, []);
-        const {
-            getTableProps,
-            getTableBodyProps,
-            headerGroups,
-            rows,
-            prepareRow,
-        } = useTable({ columns, data });
-        return (
-            <Table {...getTableBodyProps()}>
-                <Thead>
-                    {headerGroups.map((headerGroup) => (
-                        <Tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <Th {...column.getHeaderProps()}>
-                                    {column.render("Header")}
-                                </Th>
-                            ))}
+    const columnsNonMemo = Object.keys(userData[0]).map((key) => {
+        return {
+            Header: key,
+            accessor: key,
+        };
+    });
+    const columns = useMemo(() => columnsNonMemo, []);
+    const data = useMemo(() => userData, []);
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+        useTable({ columns, data });
+    return (
+        <Table {...getTableBodyProps()}>
+            <Thead>
+                {headerGroups.map((headerGroup, idx) => (
+                    <Tr key={idx} {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column, idx) => (
+                            <Th key={idx} {...column.getHeaderProps()}>
+                                {column.render("Header")}
+                            </Th>
+                        ))}
+                    </Tr>
+                ))}
+            </Thead>
+            <Tbody {...getTableBodyProps()}>
+                {rows.map((row, idx) => {
+                    prepareRow(row);
+                    return (
+                        <Tr key={idx} {...row.getRowProps()}>
+                            {row.cells.map((cell, idx) => {
+                                return (
+                                    <Td key={idx} {...cell.getCellProps()}>
+                                        {cell.render("Cell")}
+                                    </Td>
+                                );
+                            })}
                         </Tr>
-                    ))}
-                </Thead>
-                <Tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row);
-                        return (
-                            <Tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => {
-                                    return (
-                                        <Td {...cell.getCellProps()}>
-                                            {cell.render("Cell")}
-                                        </Td>
-                                    );
-                                })}
-                            </Tr>
-                        );
-                    })}
-                </Tbody>
-                <Tfoot></Tfoot>
-            </Table>
-        );
-    }
+                    );
+                })}
+            </Tbody>
+            <Tfoot></Tfoot>
+        </Table>
+    );
 }
